3ee8f30177fb33c002b4c880bfbefd99fb5e24b5
Dan Gazzola <danielrgazzola@gmail.com>
2023-01-31 16:24:12 -0500

    made changes based off Kerrin comments

--- client/src/components/ShoeTile.js
+++ client/src/components/ShoeTile.js
@@ -2,13 +2,11 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const ShoeTile = (props) => {
-    let showId = `/${props.id}`
+    const showId = `/${props.id}`
     return (
         <div className="callout">
-            <img src={props.url} width="200" height="200"/>
+            <img src={props.url} className="shoeimage"/>
             <Link to={showId}> {props.name} </Link>
-
-
         </div>
     )
 }
