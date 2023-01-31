35ad652a687af80d253fb8dbeba3500bd4189f61
Justin Laforce <jus.laforce@gmail.com>
2023-01-31 16:41:53 -0500

    Added email validation

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -27,9 +27,8 @@ class User extends uniqueFunc(Model) {
     return {
       type: "object",
       required: ["email"],
-
       properties: {
-        email: { type: "string" },
+        email: { type: "string", pattern: "^\\S+@\\S+\\.\\S+$" },
         cryptedPassword: { type: "string" },
       },
     };
