3ddfe42ab78f7c325aa8cda280d2d69bbc81570d
Dan Gazzola <danielrgazzola@gmail.com>
2023-02-03 09:53:34 -0500

    made changed based off peer review comment

--- client/src/components/ShoeShowPage.js
+++ client/src/components/ShoeShowPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import ReviewList from "./ReviewList.js"
 
-
 const ShoeShowPage = props => {
     const [shoe, setShoe] = useState({})
     const [reviews, setReviews] = useState([])
