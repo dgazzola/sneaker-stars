8b510d07fc04eef157100db48275a1763b9e7f3b
Justin Laforce <jus.laforce@gmail.com>
2023-02-07 11:27:17 -0500

    Removed comments

--- server/src/config.js
+++ server/src/config.js
@@ -2,13 +2,6 @@ import "./boot.js";
 import getNodeEnv from "./config/getNodeEnv.js";
 import getDatabaseUrl from "./config/getDatabaseUrl.cjs";
 
-// export default {
-//   nodeEnv: getNodeEnv(),
-//   session: { secret: process.env.SESSION_SECRET },
-//   databaseUrl: getDatabaseUrl(getNodeEnv()),
-//   web: { host: process.env.HOST || "0.0.0.0", port: process.env.PORT || 3000 }
-// };
-
 const development = {
   awsAccess: { key: process.env.AWS_ACCESS_KEY },
   awsSecret: { key: process.env.AWS_SECRET_ACCESS_KEY },
