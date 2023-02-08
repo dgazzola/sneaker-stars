1157a1413695034df9da1e572663536f12b09c57
Justin Laforce <jus.laforce@gmail.com>
2023-02-08 16:30:07 -0500

    Made spacing consistant across both files

--- client/src/components/ShoeShowPage.js
+++ client/src/components/ShoeShowPage.js
@@ -3,92 +3,93 @@ import { Redirect } from "react-router-dom"
 import translateServerErrors from "../services/translateServerErrors"
 
 const ShoeShowPage = props => {
-    const [shoe, setShoe] = useState({})
-    const [shouldRedirect, setShouldRedirect] = useState(false)
+  const [shoe, setShoe] = useState({})
 
-    const id = props.match.params.id
+  const [shouldRedirect, setShouldRedirect] = useState(false)
 
-    const getShoe = async () => {
-        try{
-            const response = await fetch(`/api/v1/shoes/${id}`)
-            if (!response.ok) {
-              const errorMessage = `${response.status} (${response.statusText})`
-              const error = new Error(errorMessage)
-              throw(error)
-            }
-            const shoeData = await response.json()
-            setShoe(shoeData.shoe)
-        }   catch(err) {
-            console.error(`Error in fetch: ${err.message}`)
-        }
-    }
+  const id = props.match.params.id
 
-    const deleteShoe = async () => {
-      try {
-        const response = await fetch(`/api/v1/shoes/${id}`, {
-          method: "DELETE",
-          headers: new Headers({
-            "Content-Type": "application/json"
-          }),
-          body: JSON.stringify(shoe)
-        })
+  const getShoe = async () => {
+    try {
+      const response = await fetch(`/api/v1/shoes/${id}`)
         if (!response.ok) {
-          if (response.status === 422) {
-            const body = await response.json()
-            const newErrors = translateServerErrors(body.errors)
-            return setErrors(newErrors)
-          } else {
-            const errorMessage = `${response.status} (${response.statusText})`
-            const error = new Error(errorMessage)
-            throw error
-          }
-        } else {
-          const body = await response.json()
-          setShouldRedirect(true)
+          const errorMessage = `${response.status} (${response.statusText})`
+          const error = new Error(errorMessage)
+          throw(error)
         }
-      } catch (err) {
+        const shoeData = await response.json()
+        setShoe(shoeData.shoe)
+      } catch(err) {
         console.error(`Error in fetch: ${err.message}`)
       }
-    }
+  }
 
-    useEffect(() => {
-        getShoe()
-    }, [])
-
-    const handleDelete = () => {
-      if (confirm("Are you sure you want to delete this shoe? \nThis cannot be undone") == true) {
-        deleteShoe()
+  const deleteShoe = async () => {
+    try {
+      const response = await fetch(`/api/v1/shoes/${id}`, {
+        method: "DELETE",
+        headers: new Headers({
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify(shoe)
+      })
+      if (!response.ok) {
+        if (response.status === 422) {
+          const body = await response.json()
+          const newErrors = translateServerErrors(body.errors)
+          return setErrors(newErrors)
+        } else {
+          const errorMessage = `${response.status} (${response.statusText})`
+          const error = new Error(errorMessage)
+          throw error
+        }
       } else {
-        alert("Delete Cancelled")
+        const body = await response.json()
+        setShouldRedirect(true)
       }
+    } catch (err) {
+      console.error(`Error in fetch: ${err.message}`)
     }
+  }
 
-    if (shouldRedirect) {
-      return <Redirect push to="/" />
+  useEffect(() => {
+    getShoe()
+  }, [])
+
+  const handleDelete = () => {
+    if (confirm("Are you sure you want to delete this shoe? \nThis cannot be undone") == true) {
+      deleteShoe()
+    } else {
+      alert("Delete Cancelled")
     }
+  }
 
-    let deleteButton = ''
-    if (props.user?.is_admin == true) {
-      deleteButton = <button type="button" className="button" onClick={handleDelete}>Delete Shoe</button>
-    }
+  if (shouldRedirect) {
+    return <Redirect push to="/" />
+  }
 
-    return(
-        <div className = "callout">
-            <h1>{shoe.name}</h1>
-            <img src={shoe.url} />
-            <div>
-              <h5>Category: {shoe.category}</h5>
-              <h5>Color: {shoe.color}</h5>
-              <h6>Description:</h6>
-              <p>
-                {shoe.description}
-              </p>
-              <div>
-                {deleteButton}
-              </div>
-            </div>
+  let deleteButton = ''
+  if (props.user?.is_admin == true) {
+    deleteButton = <button type="button" className="button" onClick={handleDelete}>Delete Shoe</button>
+  }
+
+  return (
+    <div className = "callout">
+      <h1>{shoe.name}</h1>
+      <img src={shoe.url} />
+      <div>
+        <h5>Category: {shoe.category}</h5>
+        <h5>Color: {shoe.color}</h5>
+        <h6>Description:</h6>
+        <p>
+          {shoe.description}
+        </p>
+        <div>
+          {deleteButton}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ShoeShowPage
--- client/src/components/UserShowPage.js
+++ client/src/components/UserShowPage.js
@@ -2,118 +2,119 @@ import React, { useState } from 'react';
 import Dropzone from "react-dropzone"
 
 const UserShowPage = (props) => {
-    const { id } = props.match.params
-    const currentUser = props.currentUser
-    const [user, setUser] = useState({
-      id: '', email: '', createdAt: '', updatedAt: '', username: '', profileImage: ''
-    })
+  const { id } = props.match.params
+  const currentUser = props.currentUser
+
+  const [user, setUser] = useState({
+    id: '', email: '', createdAt: '', updatedAt: '', username: '', profileImage: ''
+  })
     
-    const [newProfileImage, setNewProfileImage] = useState({
-      image: {}
-    })
+  const [newProfileImage, setNewProfileImage] = useState({
+    image: {}
+  })
 
-    const [uploadedImage, setUploadedImage] = useState({
-      preview: ""
-    })
+  const [uploadedImage, setUploadedImage] = useState({
+    preview: ""
+  })
 
-    const handleImageUpload = (acceptedImage) => {
-      setNewProfileImage({
-        ...newProfileImage,
-        image: acceptedImage[0]
-      })
+  const handleImageUpload = (acceptedImage) => {
+    setNewProfileImage({
+      ...newProfileImage,
+      image: acceptedImage[0]
+    })
   
-      setUploadedImage({
-        preview: URL.createObjectURL(acceptedImage[0])
-      })
-    }
+    setUploadedImage({
+      preview: URL.createObjectURL(acceptedImage[0])
+    })
+  }
 
-    const addProfileImage = async (event) => {
-      event.preventDefault()
-      const imageAddToProfile = new FormData()
-      imageAddToProfile.append("image", newProfileImage.image)
-      try {
-        const response = await fetch(`/api/v1/users/${user.id}`, {
-          method: "PATCH",
-          headers: {
+  const addProfileImage = async (event) => {
+    event.preventDefault()
+    const imageAddToProfile = new FormData()
+    imageAddToProfile.append("image", newProfileImage.image)
+    try {
+      const response = await fetch(`/api/v1/users/${user.id}`, {
+        method: "PATCH",
+        headers: {
           "Accept": "image/jpeg"
-          },
-          body: imageAddToProfile
-        })
+        },
+        body: imageAddToProfile
+      })
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`)
+      }
+      const body = await response.json()
+      setUser(body.user)
+      setUploadedImage({
+        preview: ""
+      })
+    } catch (error) {
+      console.error(`Error in add profile image: ${error.message}`)
+    }
+  }
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(`/api/v1/users/${id}`)
         if (!response.ok) {
-          throw new Error(`${response.status} (${response.statusText})`)
+          const errorMessage = `${response.status}: (${response.statusText})`
+          throw new Error(errorMessage)
         }
         const body = await response.json()
         setUser(body.user)
-        setUploadedImage({
-          preview: ""
-        })
-      } catch (error) {
-        console.error(`Error in add profile image: ${error.message}`)
-      }
+    } catch (error) {
+      console.error(`error in fetch: ${error}`)
     }
+  }
 
-    const getUser = async () => {
-      try {
-        const response = await fetch(`/api/v1/users/${id}`)
-          if(!response.ok) {
-            const errorMessage = `${response.status}: (${response.statusText})`
-            throw new Error(errorMessage)
-          }
-          const body = await response.json()
-          setUser(body.user)
-      } catch (error) {
-        console.error(`error in fetch: ${error}`)
-      }
-    }
+  useState(() => {
+    getUser()
+  }, [])
 
-    useState(() => {
-      getUser()
-    }, [])
-
-    let adminText = ""
-    if (currentUser?.is_admin) {
-      adminText = <h6>Signed in as administrator</h6>
-    }
+  let adminText = ""
+  if (currentUser?.is_admin) {
+    adminText = <h6>Signed in as administrator</h6>
+  }
     
-    let dropzoneComponent = ""
-    if(currentUser?.id === user.id){
-      dropzoneComponent = (
-        <div>
-          <h3>Click below to upload image</h3>
+  let dropzoneComponent = ""
+  if(currentUser?.id === user.id){
+    dropzoneComponent = (
+      <div>
+        <h3>Click below to upload image</h3>
 
-          <form onSubmit={addProfileImage}>
-            <Dropzone onDrop={handleImageUpload}>
-              {({getRootProps, getInputProps}) => (
-                <section>
-                  <div {...getRootProps()}>
-                    <input type="text" {...getInputProps()} />
-                    <p>input your profile image here</p>
-                  </div>
-                </section>
-              )}
-            </Dropzone>
-            <input className='button' type='submit' value='save profile' />
-          </form>
-          <img src={uploadedImage.preview} className="profile-image-preview" />
-        </div>
-      )
-    }
+        <form onSubmit={addProfileImage}>
+          <Dropzone onDrop={handleImageUpload}>
+            {({getRootProps, getInputProps}) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input type="text" {...getInputProps()} />
+                  <p>input your profile image here</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+          <input className='button' type='submit' value='save profile' />
+        </form>
+        <img src={uploadedImage.preview} className="profile-image-preview" />
+      </div>
+    )
+  }
     
-    const DateObject = new Date(user.createdAt)
-    const createdDateString= DateObject.toUTCString()
+  const DateObject = new Date(user.createdAt)
+  const createdDateString= DateObject.toUTCString()
     
-    return ( 
-        <div>
-          <div>
-              <h1>{user.username}'s Profile</h1>
-              {adminText}
-              <h4>{user.email}</h4>
-              <p>This user has been user since {createdDateString}</p>
-              <img src={user.profileImage} className='profile-image' alt='profile-image' />
-          </div>
-          {dropzoneComponent}
-        </div>
-    );
+  return ( 
+    <div>
+      <div>
+        <h1>{user.username}'s Profile</h1>
+        {adminText}
+        <h4>{user.email}</h4>
+        <p>This user has been user since {createdDateString}</p>
+        <img src={user.profileImage} className='profile-image' alt='profile-image' />
+      </div>
+      {dropzoneComponent}
+    </div>
+  );
 }
  
 export default UserShowPage;
